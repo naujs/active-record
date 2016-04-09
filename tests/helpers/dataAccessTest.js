e0b73be46a88cd5d334347a8d2b80e01af03f236
Tan Nguyen <tan@sharkpunch.com>
2016-04-09 19:03:43 +0300

    Add some integration tests

--- tests/helpers/dataAccessTest.js
+++ tests/helpers/dataAccessTest.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 global.testDataAccessMethods = function() {
   describe('.findAll', () => {
     it('should return all instances', () => {
@@ -39,13 +41,145 @@ global.testDataAccessMethods = function() {
         expect(stores.length).toEqual(3);
         var store = stores[0];
 
-        console.log(store.products());
-
         expect(store.toJSON()).toEqual({
           name: 'Store 1',
           id: 1,
           user_id: 1
         });
+
+        var products = store.products();
+        expect(products.length).toBe(3);
+
+        _.each(products, (product) => {
+          expect(product instanceof Product).toBe(true);
+        });
+      });
+    });
+
+    it('should include hasOne relation', () => {
+      return Store.findAll({
+        include: 'alias'
+      }).then((stores) => {
+        var store = stores[0];
+        var alias = store.alias();
+        expect(alias instanceof Alias).toBe(true);
+        expect(alias.toJSON()).toEqual({
+          name: 'store-1',
+          id: 1,
+          store_id: 1
+        });
+      });
+    });
+
+    it('should include belongsTo relation', () => {
+      return Store.findOne({
+        include: 'owner'
+      }).then((store) => {
+        var owner = store.owner();
+        expect(owner instanceof User).toBe(true);
+        expect(owner.toJSON()).toEqual({
+          name: 'User 1',
+          id: 1
+        });
+      });
+    });
+
+    it('should include hasManyAndBelongsTo relation', () => {
+      return Store.findOne({
+        where: {
+          name: 'Store 1'
+        },
+        include: 'tags'
+      }).then((store) => {
+        var tags = store.tags();
+        expect(tags.length).toBe(2);
+        _.each(tags, (tag) => {
+          expect(tag instanceof Tag).toBe(true);
+        });
+        expect(tags[0].toJSON()).toEqual({
+          name: 'Tag1',
+          id: 1
+        });
+        expect(tags[1].toJSON()).toEqual({
+          name: 'Tag2',
+          id: 2
+        });
+      });
+    });
+  });
+
+  describe('.findOne', () => {
+    it('should return one instance', () => {
+      return Store.findOne({
+        where: {
+          name: 'Store 1'
+        }
+      }).then((store) => {
+        expect(store.id).toEqual(1);
+      });
+    });
+  });
+
+  describe('.findByPk', () => {
+    it('should return instance matched the primary key', () => {
+      return Store.findByPk(1).then((store) => {
+        expect(store.name).toEqual('Store 1');
+      });
+    });
+  });
+
+  describe('#save', () => {
+    it('should create new instance', () => {
+      var store = new Store({
+        name: 'Store 4'
+      });
+
+      return store.save().then((savedStore) => {
+        expect(savedStore.id).toEqual(4);
+
+        return Store.findAll().then((stores) => {
+          expect(stores.length).toEqual(4);
+        });
+      });
+    });
+
+    it('should update existing instance', () => {
+      return Store.findByPk(1).then((store) => {
+        store.name = 'Store 4';
+        return store.save().then(() => {
+          return Store.findByPk(1);
+        }).then((newStore) => {
+          expect(newStore.name).toEqual('Store 4');
+        });
+      });
+    });
+  });
+
+  describe('#delete', () => {
+    it('should delete the instance', () => {
+      return Store.findByPk(1).then((store) => {
+        return store.delete();
+      }).then(() => {
+        return Store.findAll();
+      }).then((stores) => {
+        expect(stores.length).toEqual(2);
+      });
+    });
+  });
+
+  describe('.deleteAll', () => {
+    it('should delete all instances matched the criteria', () => {
+      return Store.deleteAll({
+        where: {
+          id: {
+            gte: 2
+          }
+        }
+      }).then(() => {
+        return Store.findAll();
+      }).then((stores) => {
+        expect(stores.length).toEqual(1);
+        expect(stores[0].id).toEqual(1);
       });
     });
   });
--- tests/integration/PsqlConnector.spec.js
+++ tests/integration/PsqlConnector.spec.js
@@ -1,19 +1,19 @@
-// var PsqlConnector = require('@naujs/psql-connector')
-//   , ActiveRecord = require('../../')
-//   , Registry = require('@naujs/registry');
-//
-// fdescribe('PsqlConnector', () => {
-//   var connector;
-//   beforeEach(() => {
-//     connector = PsqlConnector.getInstance(getPsqlTestDbOptions());
-//     Registry.getInstance().set('ActiveRecord.connector', connector);
-//
-//     return setupPsqlDatabase(connector.getConnection());
-//   });
-//
-//   testDataAccessMethods();
-//
-//   afterEach(() => {
-//     return teardownPsqlDatabase(connector.getConnection());
-//   });
-// });
+var PsqlConnector = require('@naujs/psql-connector')
+  , ActiveRecord = require('../../')
+  , Registry = require('@naujs/registry');
+
+describe('PsqlConnector', () => {
+  var connector;
+  beforeEach(() => {
+    connector = PsqlConnector.getInstance(getPsqlTestDbOptions());
+    Registry.getInstance().set('ActiveRecord.connector', connector);
+
+    return setupPsqlDatabase(connector.getConnection());
+  });
+
+  testDataAccessMethods();
+
+  afterEach(() => {
+    return teardownPsqlDatabase(connector.getConnection());
+  });
+});
